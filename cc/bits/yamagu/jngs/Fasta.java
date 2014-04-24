5ab330f86eb8b459f728d45588af3b11fb0af1e4
OberonTheWizard <yamagu@www4107uf.sakura.ne.jp>
2014-04-24 18:01:36 +0900

    add Fasta

---
+++ cc/bits/yamagu/jngs/Fasta.java
@@ -0,0 +1,22 @@
+package cc.bits.yamagu.jngs;
+
+public class Fasta {
+    String name;
+    String seq;
+    public Fasta(String name, String seq){
+        this.name = name;
+        this.seq = seq;
+    }
+    public String getSequence(){
+        return seq;
+    }
+    public void setSequence(String s){
+        this.seq = s;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+}
